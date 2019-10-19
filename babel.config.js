cba98c31912be2fba979be4061f92609b288d09f
Kurt Medley <kurtdmedley@gmail.com>
2019-10-19 16:29:02 -0700

    Updating packages

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,5 @@
 module.exports = {
-  presets: ["@vue/app"],
-  plugins: [
-    [
-      "@babel/transform-runtime",
-      {
-        corejs: 3
-      }
-    ],
-    [
-      "@babel/preset-env",
-      {
-        useBuiltIns: "usage",
-        corejs: 3
-      }
-    ]
+  presets: [
+    '@vue/cli-plugin-babel/preset'
   ]
-};
+}
