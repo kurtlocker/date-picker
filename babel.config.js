727c140f1f8ef605f4ea4e3f40828cca63754c69
Kurt Medley <kmedley@rei.com>
2019-10-19 15:45:28 -0700

    Initial commit

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,18 @@
 module.exports = {
-  presets: [
-    '@vue/app'
+  presets: ["@vue/app"],
+  plugins: [
+    [
+      "@babel/transform-runtime",
+      {
+        corejs: 3
+      }
+    ],
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "usage",
+        corejs: 3
+      }
+    ]
   ]
-}
+};
---
+++ src/components/Calendar.vue
@@ -0,0 +1,172 @@
+<template>
+  <section class="calendar" :data-month="date.getUTCMonth()" :data-year="date.getUTCFullYear()">
+    <header>
+      {{ month }}&nbsp;
+      <span v-if="needsYear">{{ date.getUTCFullYear() }}</span>
+    </header>
+    <main>
+      <ul>
+        <!-- Week Day Label -->
+        <li
+          class="calendar__cell calendar__cell--label"
+          v-for="(label, i) in Object.values(weekDayMap)"
+          :key="`day-abbr-${i}`"
+        >{{ label.narrow }}</li>
+        <!-- Days offset (blank grid cells) -->
+        <li v-for="(day, i) in daysOffset" :key="`day-offset-${i}`"></li>
+        <!-- Day numbers -->
+        <li
+          v-for="day in days"
+          :key="`month-day-${day}`"
+          :class="{ 
+            'calendar__cell': true,
+            'calendar__cell--past': isPast(day) 
+          }"
+          :data-day="day"
+        >{{ day }}</li>
+      </ul>
+    </main>
+    <button @click="updateMonth(-1)">Previous</button>
+    <button @click="updateMonth(1)">Next</button>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Calendar",
+  data() {
+    return {
+      today: new Date(),
+      date: this.initialDate
+    };
+  },
+  props: {
+    initialDate: {
+      type: Date,
+      required: false,
+      default: () => new Date()
+    }
+  },
+  computed: {
+    /**
+     * Returns the abbreviations of the days of the week as an array.
+     * @returns {Object}
+     */
+    weekDayMap() {
+      const baseDate = new Date(Date.UTC(2017, 0, 2)); // Random Sunday
+      const weekDayMap = {};
+      for (let i = 0; i < 7; i++) {
+        weekDayMap[this.getWeekDay(baseDate, "long")] = {
+          narrow: this.getWeekDay(baseDate, "narrow"),
+          long: this.getWeekDay(baseDate, "long"),
+          num: i
+        };
+        baseDate.setDate(baseDate.getDate() + 1);
+      }
+      return weekDayMap;
+    },
+    days() {
+      return this.getDaysInMonth(
+        this.date.getUTCFullYear(),
+        this.date.getUTCMonth()
+      );
+    },
+    daysOffset() {
+      const { date, weekDayMap } = this;
+      const monthStartDay = this.getMonthStartDay(
+        date.getUTCFullYear(),
+        date.getUTCMonth()
+      );
+      return weekDayMap[monthStartDay].num;
+    },
+    month() {
+      return this.date.toLocaleString("default", { month: "long" });
+    },
+    needsYear() {
+      const { date, today } = this;
+      return (
+        date.getUTCFullYear() > today.getUTCFullYear() ||
+        date.getUTCFullYear() < today.getUTCFullYear()
+      );
+    }
+  },
+  methods: {
+    /**
+     * Get the number of days of the given {@link month} of {@link year}.
+     * @param {Number} year
+     * @param {Number} month
+     * @returns {Number} Number of days in month
+     */
+    getDaysInMonth(year, month) {
+      return new Date(year, month + 1, 0).getDate();
+    },
+    /**
+     * Returns the localized week day name.
+     * @param {Date} date The date object
+     * @param {String} form The form we want the weekday name in
+     *  narrow = 'M'
+     *  long = 'Monday'
+     * @returns {String} The localized week day name
+     */
+    getWeekDay(date, form) {
+      return date.toLocaleDateString(undefined, { weekday: form });
+    },
+    getMonthStartDay(year, month) {
+      const date = new Date(year, month, 1);
+      return this.getWeekDay(date, "long");
+    },
+    isPast(calendarDay) {
+      const { today, date } = this;
+      const calendarYear = date.getUTCFullYear();
+      const calendarMonth = date.getUTCMonth();
+      const todayYear = today.getUTCFullYear();
+      const todayMonth = today.getUTCMonth();
+      const todayDay = today.getDate();
+      return (
+        calendarYear < todayYear ||
+        (calendarYear === todayYear && calendarMonth < todayMonth) ||
+        (calendarMonth === todayMonth && calendarDay < todayDay)
+      );
+    },
+    updateMonth(n) {
+      const { date } = this;
+      const newDate = new Date(
+        date.getUTCFullYear(),
+        date.getUTCMonth() + n,
+        1
+      );
+      this.date = newDate;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import url("//fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap");
+.calendar {
+  font-family: "Roboto", "Avenir", Helvetica, Arial, sans-serif;
+  color: #3c4043;
+  font-size: 16px;
+  main {
+    ul {
+      padding: 0;
+      margin: 0;
+      list-style: none;
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+      li {
+        padding: 2em;
+      }
+    }
+  }
+  &__cell {
+    &--label {
+      color: rgba(0, 0, 0, 0.54);
+      font-size: 13px;
+    }
+    &--past {
+      color: rgba(0, 0, 0, 0.26);
+    }
+  }
+}
+</style>
